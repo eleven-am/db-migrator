f6ba8f68c7cecd13f349e6825f1eb53726bfb9fe
Roy OSSAI <royossai@yahoo.com>
2025-07-17 14:30:40 +0200

    Bump version to v0.0.23

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version information - these can be set at build time using ldflags
 var (
-	Version   = "0.0.22"
+	Version   = "0.0.23"
 	GitCommit = "unknown"
 	BuildDate = "unknown"
 )
