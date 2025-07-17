751b014cb4d81f14c97573822fc45f6a4e617fa3
Roy OSSAI <royossai@yahoo.com>
2025-07-17 17:37:23 +0200

    Bump version to v0.0.28

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version information - these can be set at build time using ldflags
 var (
-	Version   = "0.0.27"
+	Version   = "0.0.28"
 	GitCommit = "unknown"
 	BuildDate = "unknown"
 )
